0ffde107806ed7ff597e30ba3b8d45b7bd509043
Prince Branham <priyadarshanmanoharan@gmail.com>
2023-05-21 23:58:32 +0530

    21.05.2023 Night

--- E-commerce-API/playstation.js
+++ E-commerce-API/playstation.js
@@ -1,80 +1,86 @@
-// function fetchDataAndDisplay(){
-//     fetch('http://localhost:3000/playstationCard')
-//     .then(response => response.json())
-//     .then(data => {
-//         if(data&& data.length > 0){
-//             var html = ""
+function fetchDataAndDisplay(){
+    fetch("http://localhost:3000/playstationCard")
+    .then(response => response.json())
+    .then(data => {
+        if(data&& data.length > 0){
+            var html = ""
 
-//             const objects = 
-//         }
-//     })
-// }
-
-function PlaystationCard(){
-    const xhttp = new XMLHttpRequest();
-    xhttp.open('GET','http://localhost:3000/clothes')
-    xhttp.send();
-
-    xhttp.onreadystatechange= function(){
-        if(this.readyState == 4 && this.status == 200){
-                var html = "";
-            const objects = JSON.parse(this.responseText);
-            for(let object of objects){
-             html += '<div class="container">'
-             html +=  ' <div class="row row-cols-1 row-cols-md-2 g-4 p-3">'
-             html +=    '<div class="col">'
-             html +=       '<div class="card">'
-             html +=         '<img src='+object["photo"]+' class="card-img-top "'
-             html +=           'alt="Hollywood Sign on The Hill" />'
-             html +=         '<div class="card-body">'
-             html +=          '<h5 class="card-title">'+object["GameName"]+'</h5>'
-             html +=          '<p class="card-text">'+object["description"]+'</p>'
+            for (let i = 0; i < data.length; i += 2) {
+                // Open the container div for each row
+                html += '<div class="container">';
+                html += '<div class="row row-cols-1 row-cols-md-2 g-4 p-3">';
+      
+                // Generate HTML for the first card in the row
+                html += '<div class="col">';
+                html += '<div class="card">';
+                html += '<img src="' + data[i].photo + '" class="card-img-top" alt="Card Image">';
+                html += '<div class="card-body">';
+                html += '<h5 class="card-title">' + data[i].GameName + '</h5>';
+                html += '<p class="card-text">' + data[i].description + '</p>';
+                html += '</div>';
+                html += '</div>';
+                html += '<div class="row">'
+                html +='<div class="uiverse col-lg-6">'
+                html +='<span class="tooltip">'
+                html +=  '<i class="fa-solid fa-heart"></i>'
+                html +='</span>'
+                html +='<span>'
+                html += ' Add to wishlist <i class="fa-regular fa-heart"></i>'
+                html +='</span>'
+                html +='</div>'
+                html +='<div class="uiverse cart col-lg-6">'
+                html +='<span class="tooltip"><i class="fa-solid fa-cart-plus"></i></span>'
+                html += '<span>'
+                html +=   'Add to cart <i class="fa-brands fa-opencart"></i>'
+                    html +='</span>'
+                    html +='</div>'
+                    html +='</div>'
+                html += '</div>';
+      
+                // Check if there is a second card in the row
+                if (i + 1 < data.length) {
+                  // Generate HTML for the second card in the row
+                  html += '<div class="col">';
+                  html += '<div class="card">';
+                  html += '<img src="' + data[i + 1].photo + '" class="card-img-top" alt="Card Image">';
+                  html += '<div class="card-body">';
+                  html += '<h5 class="card-title">' + data[i + 1].GameName + '</h5>';
+                  html += '<p class="card-text">' + data[i + 1].description + '</p>';
+                  html += '</div>';
+                  html += '</div>';
+                  html += '<div class="row">'
+                html +='<div class="uiverse col-lg-6">'
+                html +='<span class="tooltip">'
+                html +=  '<i class="fa-solid fa-heart"></i>'
+                html +='</span>'
+                html +='<span>'
+                html += ' Add to wishlist <i class="fa-regular fa-heart"></i>'
+                html +='</span>'
+                html +='</div>'
+                html +='<div class="uiverse cart col-lg-6">'
+                html +='<span class="tooltip"><i class="fa-solid fa-cart-plus"></i></span>'
+                html += '<span>'
+                html +=   'Add to cart <i class="fa-brands fa-opencart"></i>'
+                    html +='</span>'
+                    html +='</div>'
+                    html +='</div>'
+                  html += '</div>';
+                }
+      
+                // Close the container div for each row
+                html += '</div>';
                 
-             html +=        '</div>'
-                      html +=      '</div>'
-                      html +=   '</div>'
-                      html +=     '<div class="col">'
-                      html +=      '<div class="card">'
-                      html +=        '<img src='+object["photo"]+' class="card-img-top "'
-               html +=         'alt="Hollywood Sign on The Hill" />'
-              html +=        '<div class="card-body">'
-              html +=          '<h5 class="card-title">'+object["GameName"]+'</h5>'
-              html +=         '<p class="card-text">'+object["description"]+'</p>'
-            
-              html +=       '</div>'
-              html +=     '</div>'
-              html +=   '</div>'
-              html +=   '<div class="col">'
-              html +=       '<div class="card">'
-              html +=        '<img src='+object["photo"]+' class="card-img-top "'
-              html +=          'alt="Hollywood Sign on The Hill" />'
-              html +=        '<div class="card-body">'
-              html +=         '<h5 class="card-title">'+object["GameName"]+'</h5>'
-              html +=         '<p class="card-text">'+object["description"]+'</p>'
-                        
-                        html +=       '</div>'
-                        html +=       '</div>'
-                        html +=      '</div>'
-                  html +=     '<div class="col">'
-                  html +=      '<div class="card">'
-                  html +=        '<img src='+object["photo"]+' class="card-img-top "'
-              html +=          'alt="Hollywood Sign on The Hill" />'
-               html +=       '<div class="card-body">'
-               html +=         '<h5 class="card-title">'+object["GameName"]+'</h5>'
-               html +=        '<p class="card-text">'+object["description"]+'</p>'
-                          
-                          html +=       '</div>'
-                          html +=       '</div>'
-                          html +=    '</div>'
-                          html +=  '</div>'
-                          html +='</div>'
+                html += '</div>';
+              }
               
-            }
-           
-
-            document.getElementById("psCard").innerHTML = html;
+   
+               document.getElementById("psCard").innerHTML = html;
         }
-    }
-}
+    })
 
-PlaystationCard();
+
+    
+}
+            
+
+fetchDataAndDisplay();
